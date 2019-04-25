298a1446b726d4907f0e9411b5d8ba84ebadeaa5
Matthew Shipley <matthew.shipley@zs.com>
2019-04-25 16:15:14 -0500

    Loading visNetwork to ui.R file

--- ui.R
+++ ui.R
@@ -2,6 +2,10 @@
 # R/CFB UI SETUP #
 ##################
 
+# Loading required libraries
+library(visNetwork)
+
+# Running UI
 fluidPage(
   titlePanel("CFB Network Explorer"),
   visNetworkOutput("network")
